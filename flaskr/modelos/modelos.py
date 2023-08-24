69823372bf488c850800f231fd1e92da0db3d96b
katalina <ktorres371@soy.sena.edu.co>
2023-08-24 11:30:12 -0500

    se modifico archivo modelos de la carpeta modelos

--- flaskr/modelos/modelos.py
+++ flaskr/modelos/modelos.py
@@ -11,3 +11,31 @@ class Cancion(db.Model):
 
     def __repr__(self):
         return "{}-{}-{}-{}".format(self.titulo, self.minutos, self.segundos, self.interprete)
+
+class Usuario (db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    nombre_usuario = db.Column(db.String(128))
+    contraseña = db.Column(db.String(128))
+
+    def __repr__(self):
+        return "{}-{}-{}-{}".format(self.nombre_usuario, self.contraseña)
+
+class Album (db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    titulo = db.Column(db.String(128))
+    año = db.Column(db.Integer)
+    descripcion = db.Column(db.String(128))
+    medio = db.Column(db.String(128))
+
+    def __repr__(self):
+        return "{}-{}-{}-{}".format(self.titulo, self.año, self.descripcion, self.medio)
+
+
+class medio (db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    disco = db.Column(db.String)
+    casete = db.Column(db.String)
+    cd = db.Column(db.String)
+
+    def __repr__(self):
+        return "{}-{}-{}-{}".format(self.disco, self.casete, self.cd)
