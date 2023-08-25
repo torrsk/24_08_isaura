801a0f4684014f5273c6727b60ea3790cb5c3cfa
torrsk <ktorres371@soy.sena.edu.co>
2023-08-25 10:41:26 -0500

    se arregaron las clases actualizadas

--- flaskr/modelos/modelos.py
+++ flaskr/modelos/modelos.py
@@ -1,6 +1,8 @@
 from flask_sqlalchemy import SQLAlchemy
+import enum
 
 db = SQLAlchemy()
+
 
 class Cancion(db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -8,34 +10,52 @@ class Cancion(db.Model):
     minutos = db.Column(db.Integer)
     segundos = db.Column(db.Integer)
     interprete = db.Column(db.String(128))
+    albumes = db.relationship('Album', secondary='album_cancion', back_populates='canciones')
 
     def __repr__(self):
         return "{}-{}-{}-{}".format(self.titulo, self.minutos, self.segundos, self.interprete)
 
-class Usuario (db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    nombre_usuario = db.Column(db.String(128))
-    contraseña = db.Column(db.String(128))
 
-    def __repr__(self):
-        return "{}-{}-{}-{}".format(self.nombre_usuario, self.contraseña)
+class Medio(enum.Enum):
+    DISCO = 1
+    CASETE = 3
+    CD = 3
 
-class Album (db.Model):
+
+class Album(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     titulo = db.Column(db.String(128))
-    año = db.Column(db.Integer)
-    descripcion = db.Column(db.String(128))
-    medio = db.Column(db.String(128))
+    anio = db.Column(db.Integer)
+    descripcion = db.Column(db.String(512))
+    medio = db.Column(db.Enum(Medio))
+    usuario = db.Column(db.Integer, db.ForeignKey("usuario.id"))
+    canciones = db.relationship('Cancion', secondary='album_cancion', back_populates='albumes')
+    __table_args__ = (db.UniqueConstraint('usuario', 'titulo', name='titulo_unico_album'),)
 
-    def __repr__(self):
-        return "{}-{}-{}-{}".format(self.titulo, self.año, self.descripcion, self.medio)
 
-
-class medio (db.Model):
+class Usuario(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    disco = db.Column(db.String)
-    casete = db.Column(db.String)
-    cd = db.Column(db.String)
+    nombre_usuario = db.Column(db.String(128))
+    contrasena = db.Column(db.String)
+    albumes = db.relationship('Album', cascade='all, delete, delete-orphan')
 
-    def __repr__(self):
-        return "{}-{}-{}-{}".format(self.disco, self.casete, self.cd)
+
+albumes_canciones = db.Table('album_cancion', \
+                             db.Column('albun_id', db.Integer, db.ForeignKey('album.id'), primary_key=True), \
+                             db.Column('cancion_id', db.Integer, db.ForeignKey('cancion.id'), primary_key=True))
+
+
+class EnumADiccionario(fields.Field):
+    def _serialize(self, value, attr, obj, **kwargs):
+        if value is None:
+            return None
+        return {'llave': value.name, 'valor': value.value}
+
+
+class AlbumSchema(SQLAlchemyAutoSchema):
+    medio = EnumADiccionario(attribute=('medio'))
+
+    class Meta:
+        model = Album
+        include_relationships = True
+        load_instance = True
