f8bf28246bd81ea03db47ca6f054e151ad15e318
torrsk <ktorres371@soy.sena.edu.co>
2023-08-25 11:42:28 -0500

    se agregaron los codigos de las crud

--- flaskr/App.py
+++ flaskr/App.py
@@ -1,6 +1,8 @@
 from flaskr import create_app
 from .modelos import db, Cancion, Album, Usuario, Medio
-from .modelos import AlbumSchema
+from flask_restful import Api
+from .vistas import VistaCanciones, VistaCancion
+
 
 app = create_app('default')
 app_context = app.app_context()
@@ -9,11 +11,7 @@ app_context.push()
 db.init_app(app)
 db.create_all()
 
-# prueba
-with app.app_context():
-    AlbumSchema = AlbumSchema()
-    a = Album(titulo='chuchis', anio=2028, descripcion='chuchis', medio=Medio.CD)
-    db.session.add(a)
-    db.session.commit()
-    print(Album_Schema.dumps(album) for album in Album.query.all())
+api = Api(app)
+api.add_resource(VistaCanciones,'\canciones')
+api.add_resource(VistaCancion,'\canciones\<int:id_cancion>')
 
